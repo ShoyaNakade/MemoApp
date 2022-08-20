2106c04065fe30e386519fb64d9fc8d33e761840
Nakade Shoya <doshouya@gmail.com>
2022-08-20 15:36:12 +0900

    memo edit create screen

--- App.jsx
+++ App.jsx
@@ -2,11 +2,15 @@
 import React from 'react';
 // import MemoListScreen from './src/screens/MemoListScreen';
 // import MemoDetailScreen from './src/screens/MemoDetailScreen';
-import MemoListScreen from './src/screens/MemoListScreen';
+// import MemoListScreen from './src/screens/MemoListScreen';
+// import MemoEditScreen from './src/screens/MemoEditScreen';
+import MemoCreateScreen from './src/screens/MemoCreateScreen';
 
 export default function App() {
   return (
     // <MemoDetailScreen />
-    <MemoListScreen />
+    // <MemoListScreen />
+    // <MemoEditScreen />
+    <MemoCreateScreen />
   );
 }
