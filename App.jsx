97d801e5a7e0c016f2ca6733c5cde42eb679dc44
Nakade Shoya <doshouya@gmail.com>
2022-08-20 16:39:16 +0900

    create login and sign up screen

--- App.jsx
+++ App.jsx
@@ -4,13 +4,17 @@ import React from 'react';
 // import MemoDetailScreen from './src/screens/MemoDetailScreen';
 // import MemoListScreen from './src/screens/MemoListScreen';
 // import MemoEditScreen from './src/screens/MemoEditScreen';
-import MemoCreateScreen from './src/screens/MemoCreateScreen';
+// import MemoCreateScreen from './src/screens/MemoCreateScreen';
+// import LoginScreen from './src/screens/LoginScreen';
+import SignUpScreen from './src/screens/SignUpScreen';
 
 export default function App() {
   return (
     // <MemoDetailScreen />
     // <MemoListScreen />
     // <MemoEditScreen />
-    <MemoCreateScreen />
+    // <LoginScreen />
+    <SignUpScreen />
+    // <MemoCreateScreen />
   );
 }
